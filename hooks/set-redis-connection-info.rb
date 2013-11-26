08172e42b1fd622b5beab8345750682bc5b35765
Julian Prokay <jprokay@gmail.com>
2013-11-26 08:18:53 -0800

    Edited to remove subscribes clauses. Expects Redis env vars to be set when adding cart

--- hooks/set-redis-connection-info.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-gear_id = ARGV.shift
-domain = ARGV.shift
-ARGV.shift.split(' ').each do |env_var|
-    key = env_var.split('=')[0][2..-1]
-    val = env_var.split('=')[1]
-    File.open("env/#{key}", 'w'){ |f| f.puts val }
-end
